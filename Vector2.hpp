f0cbeb328b1eb66d9569e017bf65d39ea4d5bb9a
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-21 18:57:29 +0100

    Added commentary for each function in the library.

--- Vector2.hpp
+++ Vector2.hpp
@@ -26,10 +26,38 @@ public:
 	Vector2& operator/=(const Vector2 &right);
 
 	//functions
+	/**
+	 * @brief Change the vector length to exactly one
+	 */
 	void normalize();
+	/**
+	 * @brief Rotate the vector clockwize in the z direction
+	 * 
+	 * @param rotation Rotation angle in radians.
+	 */
 	void rotate(T rotation);
+	/**
+	 * @brief Return a vector that is the normelized vector of this vector
+	 * @details [long description]
+	 * @return this vector normilized
+	 */
 	Vector2 normalized() const;
+	/**
+	 * @brief Calcultes the length of the vector and returns it.
+	 * @details [long description]
+	 * @return The lenght of this vector
+	 */
 	T absolute() const;
+	/**
+	 * @brief Calculate the dot product of two vetors.
+	 * @details The dot product is the product of the vector in the
+	 * same length.
+	 * 
+	 * @param left Left hand side of the dot operator.
+	 * @param right Right hand side of the dot operator.
+	 * 
+	 * @return Vector result of the dot operation.
+	 */
 	static T dotProduct(const Vector2 &left, const Vector2 &right);
 };
 
